b5764dd828bfbf8d39f32211fb03ae2f6d3eaef6
likhon15 <likhon15-3916@diu.edu.bd>
2024-06-10 11:20:49 +0600

    added

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { typeDefs } from './schema';
 import { resolvers } from './resolvers';
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
 
 const main = async () => {
     const server = new ApolloServer({
@@ -10,6 +13,11 @@ const main = async () => {
     });
     const { url } = await startStandaloneServer(server, {
         listen: { port: 4000 },
+        context: async () => {
+            return {
+                prisma
+            }
+        }
     });
 
     console.log(`🚀  Server ready at: ${url}`);
